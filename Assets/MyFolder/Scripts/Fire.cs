5ff1d80167e9bde96f72bebb5610ac02fb2c7767
Skamarox <skomoroh2854@gmail.com>
2022-12-07 14:07:18 +0300

    Добавил главное меню. Добавл счетчик патронов

--- Assets/MyFolder/Scripts/Fire.cs
+++ Assets/MyFolder/Scripts/Fire.cs
@@ -1,19 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+    [SerializeField] private int _ammoCount;
     [SerializeField] private Transform _spawnPosition;
     [SerializeField] private Rigidbody _bullet;
     [SerializeField] private float _bulletForce;
     [SerializeField] private float _destroyTime;
-    
+    [SerializeField] private TMP_Text _ammoText;
+
+    private void Start()
+    {
+        _ammoText.text = $"Ammo: {_ammoCount}";
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (_ammoCount <= 0)
+                return;
             var bullet = Instantiate(_bullet, _spawnPosition.position, Quaternion.identity);
             bullet.AddForce(transform.forward * _bulletForce, ForceMode.Impulse);
             Destroy(bullet.gameObject, _destroyTime);
+            _ammoCount--;
+            _ammoText.text = $"Ammo: {_ammoCount}";
         }
     }
 }
