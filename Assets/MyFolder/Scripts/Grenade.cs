af5a3942ad8a72c8f50db0339e231b6afdd03e8a
Skamarox <skomoroh2854@gmail.com>
2022-12-07 13:36:39 +0300

    Добавил гранату и её взрыв с помощью AddExplosionForce

---
+++ Assets/MyFolder/Scripts/Grenade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPosition;
+    [SerializeField] private Rigidbody _grenade;
+    [SerializeField] private float _grenadeForwardForce;
+    [SerializeField] private float _grenadeUpForce;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            var bullet = Instantiate(_grenade, _spawnPosition.position, Quaternion.identity);
+            bullet.AddForce((transform.up * _grenadeUpForce) + (transform.forward * _grenadeForwardForce),
+                ForceMode.Impulse);
+        }
+    }
+
+}
