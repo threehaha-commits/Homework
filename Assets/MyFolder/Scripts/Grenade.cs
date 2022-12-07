cce86c5d848fc70b07446171b041210d1e941718
Skamarox <skomoroh2854@gmail.com>
2022-12-07 13:54:02 +0300

    Добавил анимацию броска гарнаты. Добавил слежение за игроком

--- Assets/MyFolder/Scripts/Grenade.cs
+++ Assets/MyFolder/Scripts/Grenade.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
@@ -6,15 +7,25 @@ public class Grenade : MonoBehaviour
     [SerializeField] private Rigidbody _grenade;
     [SerializeField] private float _grenadeForwardForce;
     [SerializeField] private float _grenadeUpForce;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            var bullet = Instantiate(_grenade, _spawnPosition.position, Quaternion.identity);
-            bullet.AddForce((transform.up * _grenadeUpForce) + (transform.forward * _grenadeForwardForce),
-                ForceMode.Impulse);
+            _animator.SetTrigger("Grenade");
         }
     }
 
+    private void CreateGranade()
+    {
+        var bullet = Instantiate(_grenade, _spawnPosition.position, Quaternion.identity);
+        bullet.AddForce((transform.up * _grenadeUpForce) + (transform.forward * _grenadeForwardForce),
+            ForceMode.Impulse);
+    }
 }
